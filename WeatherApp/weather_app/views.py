51d64de730255d73cf6b41e0949d9b8444cd8e29
sunr4zee <qwfqfqwqdqdq@gmail.com>
2023-01-24 18:42:14 +0300

    'openweathermap.org' connection

--- WeatherApp/weather_app/views.py
+++ WeatherApp/weather_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
+    appid = '431b68bea085d48610d7d1eade1b7771'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid='+ appid
+    city = 'Minsk'
     return render(request, 'weather_app/index.html')
 
 def info(request):
