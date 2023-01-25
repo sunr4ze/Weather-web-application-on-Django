ea5550933f7c0ed662d238d1de2829543a8d6fbb
sunr4zee <qwfqfqwqdqdq@gmail.com>
2023-01-25 15:14:30 +0300

    Getting weather data about the city from a request on the site

--- WeatherApp/weather_app/views.py
+++ WeatherApp/weather_app/views.py
@@ -3,8 +3,21 @@ import requests
 
 def index(request):
     appid = '431b68bea085d48610d7d1eade1b7771'
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid='+ appid
-    city = 'Minsk'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+    city = ''
+
+    tmp_request = request
+    tmp_request = str(tmp_request)
+    tmp_request = tmp_request[26:]
+
+    index = 0
+    while tmp_request[index] != '&':
+        city += tmp_request[index]
+        index += 1
+
+    res = requests.get(url.format(city)).json()
+    print(res)
+
     return render(request, 'weather_app/index.html')
 
 def info(request):
